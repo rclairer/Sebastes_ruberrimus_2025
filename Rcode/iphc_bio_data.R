6d6d4014d265d98dd8f40bcc8d48020bbb5951e9
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2025-02-10 10:31:33 -0500

    move files to new structure

--- Rcode/iphc_bio_data.R
+++ Rcode/iphc_bio_data.R
@@ -3,13 +3,13 @@
 library(dplyr)
 library(tidyr)
 
-iphc_bio <- read.csv(file.path(getwd(), "Data", "iphc_biodata.csv"))
+iphc_bio <- read.csv(file.path(getwd(), "Data", "processed", "iphc_biodata.csv"))
 
 ##### Length Comps #####
 # method used for assigning bins in nwfscSurvey package
-l_bins <- c(10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50,52,54,56,58,60,62,64,66,68,70,72,74)
+l_bins <- c(10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 54, 56, 58, 60, 62, 64, 66, 68, 70, 72, 74)
 bins <- c(l_bins, Inf)
-iphc_bio$l_bin <- as.numeric(bins[findInterval(iphc_bio[,"fish_length_cm"], bins, all.inside = T)])
+iphc_bio$l_bin <- as.numeric(bins[findInterval(iphc_bio[, "fish_length_cm"], bins, all.inside = T)])
 
 # get length comps by year and bin
 length_comps <- iphc_bio |>
@@ -24,9 +24,11 @@ write.csv(length_comps, file.path(getwd(), "Data", "iphc_length_comps.csv"), row
 
 # get just those until 2016 so it can be compared to the spreadsheet Jason sent me
 test_against_2017 <- length_comps |>
-        dplyr::filter(sample_year < 2017) |>
-        dplyr::mutate(sample_year = sample_year,
-                      sum = rowSums(across(where(is.numeric))))
+  dplyr::filter(sample_year < 2017) |>
+  dplyr::mutate(
+    sample_year = sample_year,
+    sum = rowSums(across(where(is.numeric)))
+  )
 
 View(test_against_2017)
 
@@ -43,19 +45,16 @@ caal <- iphc_bio |>
   dplyr::arrange(a_bin) |>
   tidyr::pivot_wider(names_from = "a_bin", values_from = "n", values_fill = 0) |>
   dplyr::arrange(sample_year, l_bin) |>
-  dplyr::mutate(month = 7,
-                fleet = 12,
-                sex = 0,
-                part = 0,
-                ageerr = 1,
-                Lbin_lo = l_bin,
-                Lbin_hi = l_bin,
-                Nsamp = rowSums(across(`-999`:`65`))) |>
+  dplyr::mutate(
+    month = 7,
+    fleet = 12,
+    sex = 0,
+    part = 0,
+    ageerr = 1,
+    Lbin_lo = l_bin,
+    Lbin_hi = l_bin,
+    Nsamp = rowSums(across(`-999`:`65`))
+  ) |>
   dplyr::select(sample_year, month, fleet, sex, part, ageerr, Lbin_lo, Lbin_hi, Nsamp, everything())
 
-write.csv(caal, file.path(getwd(), "Data", "iphc_caal.csv"), row.names = FALSE)
-
-
-
-
-
+write.csv(caal, file.path(getwd(), "Data", "processed", "iphc_caal.csv"), row.names = FALSE)
