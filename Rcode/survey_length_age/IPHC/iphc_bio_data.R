0e8dc660e6eb0452265ebff6342f0479ad73f028
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2025-02-18 16:35:50 -0500

    add previous code back in commented out

--- Rcode/survey_length_age/IPHC/iphc_bio_data.R
+++ Rcode/survey_length_age/IPHC/iphc_bio_data.R
@@ -15,6 +15,25 @@ iphc_bio$l_bin <- as.numeric(bins[findInterval(iphc_bio[, "fish_length_cm"], bin
 
 # get length comps by year and bin
 # unable to provide nsamps yet until Fabio gets the stlkey
+
+# Previous method was just the following
+# length_comps <- iphc_bio |>
+#   dplyr::filter(!is.na(l_bin)) |>
+#   dplyr::group_by(sample_year, l_bin) |>
+#   dplyr::summarize(n = n()) |>
+#   dplyr::arrange(l_bin) |>
+#   tidyr::pivot_wider(names_from = "l_bin", values_from = "n", values_fill = 0) |>
+#   dplyr::arrange(sample_year) |>
+#   dplyr::mutate(
+#     month = 7,
+#     fleet = 12,
+#     sex = 0,
+#     part = 0,
+#     Nsamp = NA
+#   ) |>
+#   dplyr::select(sample_year, month, fleet, sex, part, Nsamp, everything()) |>
+#   dplyr::rename(year = sample_year)
+
 length_comps <- iphc_bio |>
   dplyr::filter(!is.na(best_age)) |>
   dplyr::filter(!is.na(l_bin)) |>
@@ -108,6 +127,7 @@ maal <- iphc_bio |>
 write.csv(maal, file.path(getwd(), "Data", "processed", "IPHC_bio_data", "iphc_marginal_ages.csv"), row.names = FALSE)
 
 # Comparison plots
+# IPHC lengths
 # Read new data
 iphc_lengths_new <- read.csv(file.path(getwd(), "Data", "processed", "IPHC_bio_data", "iphc_length_comps.csv")) |>
   mutate(assessment = "Current") |>
@@ -143,6 +163,43 @@ comparison_plot <- together |>
   ggplot2::ggplot(aes(x = year, y = length, col = assessment, size = freq)) +
   ggplot2::geom_point(position = position_dodge(0.5))
 ggsave(plot = comparison_plot, "iphc_length_comp_comparisons.png", path = file.path(getwd(), "Rcode", "survey_length_age", "IPHC"))
+
+
+# Previous method used to do comparison plots
+# iphc_lengths <- read.csv(file.path(getwd(), "Rcode", "survey_length_age", "IPHC", "iphc_comparison_lengths.csv"))
+# 
+# long_iphc_lengths <- iphc_lengths |>
+#   dplyr::mutate(type = dplyr::case_when(
+#     Fleet == 12 ~ "previous assessment",
+#     Fleet == 13 ~ "current assessment"
+#   )) |>
+#   dplyr::select(Year, type, Nsamps, `X10`:`X74`) |>
+#   tidyr::pivot_longer(cols = c(-Year, -type, -Nsamps), names_to = "length", values_to = "freq") |>
+#   dplyr::mutate(length = gsub("X", "", length))
+# 
+# old_comps <- long_iphc_lengths |>
+#   dplyr::filter(type == "previous assessment") |>
+#   dplyr::group_by(Year) |>
+#   dplyr::mutate(
+#     freq = freq / sum(freq),
+#     # NsampsFreq = freq/unique(Nsamps), still waiting on these
+#     length = as.numeric(length)
+#   )
+# new_comps <- long_nwfsc_lengths |>
+#   dplyr::filter(type == "current assessment") |>
+#   dplyr::group_by(Year) |>
+#   dplyr::mutate(
+#     freq = freq / sum(freq),
+#     # NsampsFreq = freq/unique(Nsamps), still waiting on these
+#     length = as.numeric(length)
+#   )
+# 
+# together <- rbind(old_comps, new_comps)
+# comparison_plot <- together |>
+#   dplyr::filter(freq > 0) |>
+#   ggplot2::ggplot(aes(x = Year, y = length, col = type, size = freq)) +
+#   ggplot2::geom_point(position = position_dodge(0.5))
+# ggsave(plot = comparison_plot, "iphc_length_comp_comparisons.png", path = file.path(getwd(), "Rcode", "survey_length_age", "IPHC"))
 
 ### IPHC ages
 iphc_ages <- read.csv(file.path(getwd(), "Rcode", "survey_length_age", "IPHC", "iphc_comparison_ages.csv"))
