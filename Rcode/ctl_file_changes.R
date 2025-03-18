5b1ada0fd3fe456e94821877b247f90d29b179f0
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2025-03-18 14:21:47 -0400

    update

--- Rcode/ctl_file_changes.R
+++ Rcode/ctl_file_changes.R
@@ -14,8 +14,8 @@ ctl <- r4ss::SS_readctl(file = file.path(model_2017_path, "control.ss_new"))
 
 ctl$MG_parms["Wtlen_1_Fem_GP_1",]$INIT <- 7.183309e-06
 ctl$MG_parms["Wtlen_1_Fem_GP_1",]$PRIOR <- 7.183309e-06
-ctl$MG_paras["Wtlen_2_Fem_GP_1",]$INIT <- 3.244801
-ctl$MG_paras["Wtlen_2_Fem_GP_1",]$PRIOR <-3.244801
+ctl$MG_parms["Wtlen_2_Fem_GP_1",]$INIT <- 3.244801
+ctl$MG_parms["Wtlen_2_Fem_GP_1",]$PRIOR <-3.244801
 
 # Fill outfile with directory and file name of the file written
 r4ss::SS_writectl(ctl, outfile = "", overwrite = TRUE)
