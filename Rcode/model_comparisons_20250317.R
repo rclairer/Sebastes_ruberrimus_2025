debdd8f5dc38e05f8891ad5581cf032f14a506a7
Claire Rosemond <claire.rosemond@noaa.gov>
2025-03-20 22:35:08 -0700

    model comparison runs with updated indices

--- Rcode/model_comparisons_20250317.R
+++ Rcode/model_comparisons_20250317.R
@@ -13,13 +13,40 @@ model_2017_base_path <- file.path(getwd(), "model", "2017_yelloweye_model_base")
 
 model_2017_updatedexe_path <- file.path(getwd(), "model", "2017_yelloweye_model_updated_ss3_exe")
 
+###########################################################################
+############   UPDATED SS3 EXE but nsexes same as base 2017   #############
+###########################################################################
 
-##############################################
-#### UPDATED SS3 EXE, UPDATED CATCH #########
-#############################################
+#model_2017_updatedexe_nsexes1
 
 copy_SS_inputs(
-  dir.old = model_2017_updatedexe_path, #why won't this work with the biasadj model
+  dir.old = model_2017_updatedexe_path, 
+  dir.new = file.path(getwd(), "model", "2017_updated_ss3_exe_nsexes1_20250320"),
+  create.dir = FALSE,
+  overwrite = TRUE,
+  use_ss_new = TRUE,
+  verbose = TRUE
+)
+
+inputs_nsexes <- SS_read(dir = file.path(getwd(), "model", "2017_updated_ss3_exe_nsexes1_20250320"))
+inputs_nsexes$dat$Nsexes <- 1  
+
+SS_write(inputs_nsexes, dir = file.path(getwd(), "model", "2017_updated_ss3_exe_nsexes1_20250320"), overwrite = TRUE)
+
+get_ss3_exe(dir = file.path(getwd(), "model", "2017_updated_ss3_exe_nsexes1_20250320"))
+
+run(dir = file.path(getwd(), "model", "2017_updated_ss3_exe_nsexes1_20250320"), show_in_console = TRUE)
+
+replist <- SS_output(dir = file.path(getwd(), "model", "2017_updated_ss3_exe_nsexes1_20250320"))
+SS_plots(replist)
+
+
+#####################################################################
+#### UPDATED SS3 EXE, UPDATED CATCH (keep < 2017 the same) #########
+#####################################################################
+
+copy_SS_inputs(
+  dir.old = model_2017_updatedexe_path, 
   dir.new = file.path(getwd(), "model", "2025_updated_catch_20250317"),
   create.dir = FALSE,
   overwrite = TRUE,
@@ -293,6 +320,7 @@ run(dir = file.path(getwd(), "model", "2025_updated_catch_20250317"), show_in_co
 replist <- SS_output(dir = file.path(getwd(), "model", "2025_updated_catch_20250317"))
 SS_plots(replist)
 
+
 ###################################################
 # UPDATED SS3, UPDATED CATCH, UPDATED WCGBTS INDEX#
 ###################################################
@@ -343,7 +371,7 @@ SS_plots(replist)
 
 copy_SS_inputs(
   dir.old = model_2017_updatedexe_path,
-  dir.new = file.path(getwd(), "model", "2025_updated_catch_indices_20250317"),
+  dir.new = file.path(getwd(), "model", "2025_updated_catch_indices_20250320"),
   create.dir = FALSE,
   overwrite = TRUE,
   use_ss_new = TRUE,
@@ -425,14 +453,206 @@ all_indices <- do.call("rbind", list(
 
 inputs_catch_indices$dat$indices <- all_indices
 
-SS_write(inputs_catch_indices, dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250317"), overwrite = TRUE)
+SS_write(inputs_catch_indices, dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250320"), overwrite = TRUE)
 
-get_ss3_exe(dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250317"))
+get_ss3_exe(dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250320"))
 
-run(dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250317"), show_in_console = TRUE)
+run(dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250320"), show_in_console = TRUE)
 
-replist <- SS_output(dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250317"))
+replist <- SS_output(dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250320"))
 SS_plots(replist)
+
+##############################################################
+# UPDATED SS3, UPDATED CATCH, UPDATED ALL INDICES, some comps#
+##############################################################
+copy_SS_inputs(
+  dir.old = model_2017_updatedexe_path,
+  dir.new = file.path(getwd(), "model", "2025_updated_catch_indices_somecomps_20250320"),
+  create.dir = FALSE,
+  overwrite = TRUE,
+  use_ss_new = TRUE,
+  verbose = TRUE
+)
+
+# Use ss_new files from the catches run
+inputs_catch_indices_somecomps <- SS_read(dir = file.path(getwd(), "model", "2025_updated_catch_indices_20250320"), ss_new = TRUE)
+
+
+#just update Juliette's
+
+###########################
+### Length compositions ###
+###########################
+
+colnames_l <- colnames(inputs$dat$lencom)
+
+# CA TWL (from PacFIN) - fleet 1
+
+# CA NONTWL (from PacFIN) - fleet 2
+
+# CA NONTWL (from WCGOP) - fleet 2
+
+# CA REC - fleet 3
+CA_REC_lengths <- read.csv(file.path(getwd(), "Data", "processed", "recfin_bio_data", "recfin_ca_lengths.csv"))
+colnames(CA_REC_lengths) <- colnames_l
+
+# ORWA TWL (PacFIN and WCGOP combined) - fleet 4
+
+# ORWA NONTWL (PacFIN and WCGOP combined) - fleet 5
+
+# OR REC (MRFSS and ORBS combined, plus data associated with WDFW ages (1979-2002) and ODFW (2009-2016) ages, not included in RecFIN) - fleet 6
+
+# WA REC (data from WDFW) - fleet 7
+CA_REC_lengths <- read.csv(file.path(getwd(), "Data", "processed", "recfin_bio_data", "recfin_wa_lengths.csv"))
+colnames(CA_REC_lengths) <- colnames_l
+
+# CA observer - fleet 8
+
+# OR observer - fleet 9
+
+# Nsamp method used for all survey length comps is the old Stewart Hamel method from the 2017 assessment where
+# Nsamp = n_trips + 0.0707 * n_fish when n_fish/n_tows < 55 and
+# Nsamp = 4.89 * n_trips when n_fish/n_tows >= 55
+# Triennial survey - fleet 10
+tri_lengths <- read.csv(file.path(
+  getwd(), "Data", "processed", "NWFSC.Combo_and_Tri_length_comps",
+  "Tri_length_cm_unsexed_raw_10_74_yelloweye rockfish_groundfish_triennial_shelf_survey.csv"
+)) |>
+  colnames(tri_lengths) <- colnames_l
+
+# NWFSC survey - fleet 11
+nwfsc_lengths <- read.csv(file.path(
+  getwd(), "Data", "processed", "NWFSC.Combo_and_Tri_length_comps",
+  "NWFSC.Combo_length_cm_unsexed_raw_10_74_yelloweye rockfish_groundfish_slope_and_shelf_combination_survey.csv"
+)) |>
+  colnames(nwfsc_lengths) <- colnames_l
+
+# IPHC ORWA - fleet 12
+# IPHC bio data notes:
+# Total_Biodata_Comb includes all Yelloweye biodata collected from IPHC FISS 2A from 2022-2023 and stlkeys for association with the IPHC effort database, and some location information pulled from the IPHC effort database
+# Experimental gear catch and catch where species could not be rectified against onboard tag documentation were removed.
+# 2A was not fished in 2020 and 2024.  Oregon stations were not fished in 2023.
+# Oregon rockfish were not tagged in 2021 and fish cannot be reconciled with IPHC effort data.
+# IPHC has not provided onboard tag information for Oregon stations in 2019. 2019 landings currently cannot be reconciled with IPHC effort data.
+
+iphc_lengths <- read.csv(file.path(
+  getwd(),
+  "Data", "processed", "IPHC_bio_data", "iphc_length_comps.csv"
+))
+
+
+all_lengths <- do.call("rbind", list(
+  tri_lengths,
+  nwfsc_lengths,
+  iphc_lengths
+))
+
+inputs_catch_indices_somecomps$dat$lencomp <- all_lengths
+
+########################
+### Age compositions ###
+########################
+
+colnames_a <- colnames(inputs$dat$agecom)
+
+# CA NONTWL CAAL - fleet 2
+
+# CA NONTWL MAAL - fleet -2
+
+# CA NONTWL WCGOP - fleet -2 and 2
+ca_nontwl_wcgop <- inputs$dat$agecom |>
+  filter(fleet %in% c(-2, 2)) |>
+  filter(year == 2005)
+colnames(ca_nontwl_wcgop) <- colnames_a
+
+# CA REC CAAL and MAAL (aged by WDFW, 1983 and 1996 only) - fleet -3 and 3
+ca_rec_wdfw <- inputs$dat$agecom |>
+  filter(fleet %in% c(-3, 3))
+ca_rec_wdfw <- ca_rec_wdfw[1:4, ]
+colnames(ca_rec_wdfw) <- colnames_a
+
+# CA REC CAAL (data from Don Pearson 1979 - 1984, aged by Betty) - fleet -3 and 3
+ca_rec_don_pearson <- inputs$dat$agecom |>
+  filter(fleet %in% c(-3, 3))
+ca_rec_don_pearson <- ca_rec_don_pearson[-c(1:4), ] |>
+  filter(year < 1985)
+colnames(ca_rec_don_pearson) <- colnames_a
+
+# CA REC CAAL (data from CDFW John Budrick, aged by Betty) - fleet 3
+
+# CA REC MAAL (data from John Budrick, aged by Betty) - fleet -3
+
+# ORWA TWL CAAL (PacFIN and WCGOP combined) - fleet 4
+
+# ORWA TWL MAAL (PacFIN and WCGOP combined) - fleet -4
+
+# ORWA NONTWL CAAL (PacFIN and WCGOP combined) - fleet 5
+
+# ORWA NONTWL MAAL (PacFIN and WCGOP combined) - fleet -5
+
+# OR REC CAAL - fleet 6
+
+# OR REC MAAL - fleet -6
+
+# WA REC CAAL - fleet 7
+
+# WA REC MAAL - fleet -7
+
+# Nsamp method used for all survey maal is the old Stewart Hamel method from the 2017 assessment where
+# Nsamp = n_trips + 0.0707 * n_fish when n_fish/n_tows < 55 and
+# Nsamp = 4.89 * n_trips when n_fish/n_tows >= 55
+# NWFSC survey CAAL and MAAL - fleet -11 and 11
+nwfsc_caal <- read.csv(file.path(
+  getwd(), "Data", "processed", "NWFSC.Combo_CAAL",
+  "processed_one_sex_caal.csv"
+))
+colnames(nwfsc_caal) <- colnames_a
+
+nwfsc_maal <- read.csv(file.path(
+  getwd(), "Data", "processed", "NWFSC.Combo_age_comps",
+  "NWFSC.Combo_age_unsexed_raw_0_65_yelloweye rockfish_groundfish_slope_and_shelf_combination_survey.csv"
+)) |>
+  mutate(
+    ageerr = 2,
+    fleet = -11
+  )
+
+colnames(nwfsc_maal) <- colnames_a
+
+nwfsc_ages <- rbind(nwfsc_caal, nwfsc_maal)
+
+# IPHC survey CAAL and MAAL - fleet -12 and 12
+iphc_caal <- read.csv(file.path(
+  getwd(),
+  "Data", "processed", "IPHC_bio_data", "iphc_caal.csv"
+))
+iphc_maal <- read.csv(file.path(
+  getwd(),
+  "Data", "processed", "IPHC_bio_data", "iphc_marginal_ages.csv"
+))
+iphc_ages <- rbind(iphc_caal, iphc_maal)
+
+# Combine all ages together
+all_ages <- do.call("rbind", list(
+  ca_nontwl_wcgop,
+  ca_rec_wdfw,
+  ca_rec_don_pearson,
+  nwfsc_ages,
+  iphc_ages
+))
+
+inputs_catch_indices_somecomps$dat$agecomp <- all_ages
+
+SS_write(inputs_catch_indices, dir = file.path(getwd(), "model", "2025_updated_catch_indices_somecomps_20250320"), overwrite = TRUE)
+
+get_ss3_exe(dir = file.path(getwd(), "model", "2025_updated_catch_indices_somecomps_20250320"))
+
+run(dir = file.path(getwd(), "model", "2025_updated_catch_indices_somecomps_20250320"), show_in_console = TRUE)
+
+replist <- SS_output(dir = file.path(getwd(), "model", "2025_updated_catch_indices_somecomps_20250320"))
+SS_plots(replist)
+
+
 
 #####################################################
 ############## MODEL COMPARISON  ###################
@@ -442,9 +662,11 @@ SS_plots(replist)
 # Get branch that SSsummarize fix is on for now until it is merged into main
 #devtools::install_github("r4ss/r4ss", ref = "fix_SSsummarize")
 # library(r4ss)
+
+#not using
 models <- list.dirs(file.path(getwd(), "model"), recursive = FALSE)
 #first, base, updated ss3 exe
-models <- models[c(2,3)]
+models <- models[c(3,4)] #CHECK THIS EVERY TIME
 models_output <- SSgetoutput(dirvec = models)
 models_summary <- SSsummarize(models_output)
 SSplotComparisons(models_summary,
@@ -453,8 +675,21 @@ SSplotComparisons(models_summary,
                   print = TRUE
 )
 
+
 models <- list.dirs(file.path(getwd(), "model"), recursive = FALSE)
-models <- models[c(2,3,5,6)]
+#first, base, updated ss3 exe with nsexes = 1 and then with nsexes = -1
+models <- models[c(3,2,4)] #CHECK THIS EVERY TIME
+models_output <- SSgetoutput(dirvec = models)
+models_summary <- SSsummarize(models_output)
+SSplotComparisons(models_summary,
+                  plotdir = file.path(getwd(), "Rcode", "SSplotComparisons_output", "term1_final_model_comparisons", "base_updatedss3exe_nsexes"),
+                  legendlabels = c("2017 base", "2017 updated SS3 exe (Nsexes = 1)", "2017 updated SS3 exe (Nsexes = -1)"),
+                  print = TRUE
+)
+
+#not using
+models <- list.dirs(file.path(getwd(), "model"), recursive = FALSE)
+models <- models[c(2,3,5,6)] #CHECK THIS EVERY TIME
 models_output <- SSgetoutput(dirvec = models)
 models_summary <- SSsummarize(models_output)
 SSplotComparisons(models_summary,
@@ -463,5 +698,14 @@ SSplotComparisons(models_summary,
                   print = TRUE
 )
 
+models <- list.dirs(file.path(getwd(), "model"), recursive = FALSE)
+models <- models[c(4,6,8)] #CHECK THIS EVERY TIME
+models_output <- SSgetoutput(dirvec = models)
+models_summary <- SSsummarize(models_output)
+SSplotComparisons(models_summary,
+                  plotdir = file.path(getwd(), "Rcode", "SSplotComparisons_output", "term1_final_model_comparisons", "updatedss3exe_updatedcatch_updatedindices"),
+                  legendlabels = c("2017 updated SS3 exe (Nsexes = -1)", "2025 updated catch", "2025 updated catch & indices"),
+                  print = TRUE
+)
 
 
