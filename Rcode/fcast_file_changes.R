e771bd57fa99335301e39fd36a46ade1259b7c65
Elizabeth Perl <elizabeth.gugliotti@noaa.gov>
2025-04-08 13:38:09 -0400

    ctl file changes and run models

--- Rcode/fcast_file_changes.R
+++ Rcode/fcast_file_changes.R
@@ -8,17 +8,15 @@ library(tidyr)
 library(r4ss)
 library(here)
 
-inputs <-SS_read(dir = update_ctl_model_path)
+inputs <- SS_read(dir = update_ctl_model_path)
 
 fcast <- inputs$fore
-
 
 # Update flimit fraction
 fcast$Flimitfraction <- -1
 
 # update buffer values
 remotes::install_github("pfmc-assessments/PEPtools")
-PEPtools::get_buffer(years = 2025:2036, sigma =  0.5, pstar = 0.45)
+PEPtools::get_buffer(years = 2025:2036, sigma = 0.5, pstar = 0.45)
 
 fcast$stddev_of_log_catch_ratio <- 0
-
